78415bf0938366ba71ccd90b3405b9db408a43e6
Scout <Scout.a.miller@gmail.com>
2025-11-16 00:55:39 -0800

    pause menu

---
+++ Scripts/pause_ui.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@export var Menu: PackedScene
+
+func _on_button_pressed() -> void:
+	visible = false
+
+
+func _on_quit_pressed() -> void:
+	get_tree().change_scene_to_packed(Menu)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Pause"):
+		visible = !visible

40e0e085dc36169a14320d6990482e5feba9f881
Scout <Scout.a.miller@gmail.com>
2025-11-15 23:25:07 -0800

    SCENE CHANGES

--- Scripts/guard.gd
+++ Scripts/guard.gd
@@ -1,11 +1,15 @@
 extends "res://Scripts/unit.gd"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+var idle = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	try_new_move()
+
+func try_new_move():
+	if walking == false && idle == false:
+		idle = true
+		await get_tree().create_timer(3).timeout
+		idle = false
+		set_new_target_pos(map.local_to_map(position) + Vector2i(randi_range(-5, 5), randi_range(-5, 5)))
--- Scripts/unit.gd
+++ Scripts/unit.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
 var map
+var walking
 
 func _ready() -> void:
 	map = get_node("/root/Level/TileMapLayer")
 
 	
 func set_new_target_pos(new_pos: Vector2):
+	if walking:
+		return
+	walking = true
 	var path = map.get_path_to_target(map.local_to_map(position), map.local_to_map(new_pos))
 	for cell in path:
 		position = map.map_to_local(cell)
 		await get_tree().create_timer(0.2).timeout
+	walking = false
 
 
 
